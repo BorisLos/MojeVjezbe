a661c1b97f9270baab16353c057c59280faaa3b4
Boris Lasović <57643982+BorisLos@users.noreply.github.com>
2019-12-16 15:09:20 +0100

    Update KoordSustav.java

--- KoordSustav.java
+++ KoordSustav.java
@@ -5,18 +5,16 @@ import javax.swing.JOptionPane;
 public class KoordSustav {
 
 	public static void main(String[] args) {
-		int a1=Integer.parseInt(JOptionPane.showInputDialog("Unesi x toèke A "));
-		int a2=Integer.parseInt(JOptionPane.showInputDialog("Unesi y toèke A "));
-		int b1=Integer.parseInt(JOptionPane.showInputDialog("Unesi x toèke B "));
-		int b2=Integer.parseInt(JOptionPane.showInputDialog("Unesi x toèke B "));
-		
-		int a=Math.abs(a1-a2);
-		int b=Math.abs(b1-b2);
-		
-		double d=Math.sqrt((a*a)+(b*b));
-		 System.out.println(d);
-		
-		
+		int a1 = Integer.parseInt(JOptionPane.showInputDialog("Unesi x toèke A "));
+		int a2 = Integer.parseInt(JOptionPane.showInputDialog("Unesi y toèke A "));
+		int b1 = Integer.parseInt(JOptionPane.showInputDialog("Unesi x toèke B "));
+		int b2 = Integer.parseInt(JOptionPane.showInputDialog("Unesi x toèke B "));
+
+		int a = Math.abs(a1 - b1);
+		int b = Math.abs(a1 - b2);
+
+		double d = Math.sqrt((a * a) + (b * b));
+		System.out.println(d);
 
 	}
 
